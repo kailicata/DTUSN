9cc938f0b8f087fb044f07d9f0f839ed7e4437bd
kai licata <kailicata@gmail.com>
2025-01-05 17:21:43 -0800

    model paramters

---
+++ model_parameters.py
@@ -0,0 +1,25 @@
+#parameters for neuron and ultrasound simulation model
+
+model_parameters ={
+
+"soma_length" : 12.6157, #length of soma (meters)
+
+"soma_diameter" : 12.6157, #diameter of soma (meters)
+
+"dend_length" : 200, #length of dendrite (meters)
+ 
+"dend_diameter" : 1, #diameter of dendrite (meters)
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
