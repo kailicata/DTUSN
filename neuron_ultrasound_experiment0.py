8b5169e982ee0a423b3a9d2c021db87f2fe3d327
kai licata <kailicata@gmail.com>
2025-04-19 20:52:24 -0700

    comments

--- neuron_ultrasound_experiment0.py
+++ neuron_ultrasound_experiment0.py
@@ -1,11 +1,18 @@
-from hashlib import file_digest
+# Import neuron model classes
 from neuron_model1 import Interneuron_Group, NeuronCell_Morphology_Biophysics, Cell_Geometry
-from model_parameters import model_parameters as mp
+# Import simulation parameters
+from model_parameters import model_parameters as mp# Import neuron visualization function
 from neuron_ultrasound_model_plot import neuron_plot
+# Import utility to load 3D confocal coordinates for soma/dendrite
 from general_purpose_model_utilities import load_extracted_cell_coordinates
 
+
+
+# Load preprocessed morphology data from confocal imaging
 cell_data = load_extracted_cell_coordinates()
-
-ring = Interneuron_Group(mp,N=6, cell_data=cell_data)
+# Create a ring network of 6 neurons using the confocal morphology
+def run_simulation(params):
+    ring = Interneuron_Group(params["model"], N=params["N"], cell_data=params["cell_data"])
+    return ring
+# Plot the 3D morphology of the first neuron in the ring
 neuron_plot(ring.cells[0])
-
