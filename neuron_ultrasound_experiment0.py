1910bda802eabc6b66e9f5717c6957e7f9d22b2c
kai licata <kailicata@gmail.com>
2025-07-19 17:00:36 -0700

    merge conflict

--- neuron_ultrasound_experiment0.py
+++ neuron_ultrasound_experiment0.py
@@ -1,7 +1,7 @@
 # Import neuron model classes
 from neuron_model1 import Interneuron_Group, NeuronCell_Morphology_Biophysics, Cell_Geometry
 # Import simulation parameters
-from model_parameters import model_parameters as mp # Import neuron visualization function
+from model_parameters import model_parameters as params # Import neuron visualization function
 from neuron_ultrasound_model_plot import neuron_plot
 # Import utility to load 3D confocal coordinates for soma/dendrite
 from general_purpose_model_utilities import load_extracted_cell_coordinates
@@ -15,8 +15,8 @@ def run_simulation(params):
     ring = Interneuron_Group(params["model"], N=params["N"], cell_data=params["cell_data"])
     return ring
 # Plot the 3D morphology of the first neuron in the ring
-"""
+
 ring = run_simulation(params)
-"""
+
 
 neuron_plot(ring.cells[0])
